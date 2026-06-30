51c2b8a48f42d18011ca4a17694b56e883112f57
sachin <sachinnarofficials@gmail.com>
2026-06-30 12:39:50 +0530

    react and openAI in progress

---
+++ gen-ai-projects/ai-assistant/chat-bot/src/App.tsx
@@ -0,0 +1,10 @@
+
+export const App: React.FunctionComponent = () => {
+  return (
+    <div className="max-w-lg mt-20 mx-auto bg-white shadow-md rounded">
+      <div className="w-full max-w-lg bg-white shadow-lg rounded-lg">
+        Test
+      </div>
+    </div>
+  )
+}
